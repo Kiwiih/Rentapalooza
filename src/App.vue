5d3091cefbd6f8273c32a4a41d29d186392a4daa
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-12-06 11:37:11 +0100

    hämtade saker från apiet

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
-  import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router'
+import AvailableItems from "./components/AvailableItems.vue"
 </script>
 
 <template>
+  <!-- VIKTORSTESTS -->
+  <AvailableItems />
+  <!-- VIKTORSTESTS -->
   <RouterView />
 </template>
 
---
+++ src/components/AvailableItems.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { useItems } from "@/shared/useItems.js";
+import { onMounted } from 'vue'
+
+
+// Anropa `useItems` för att få tillgång till `items` och `getItems`
+const { items, getItems } = useItems();
+
+
+</script>
+
+<template>
+    <div>
+        funktion hör: {{ getItems() }}
+        <br>
+        Variabel här: {{ items }}
+    </div>
+
+    <div>
+        Detta är en lista med tillgängliga items
+    </div>
+
+    <div>
+    </div>
+</template>
