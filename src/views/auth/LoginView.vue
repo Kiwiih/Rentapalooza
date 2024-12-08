b35fcd68171089c71d6ec0a6d8df62e003d63e40
jerry <jerry.olsson@chasacademy.se>
2024-12-08 12:19:18 +0100

    Register and login is now avalible

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,7 +1,71 @@
-<script setup></script>
+<!-- LoginView.vue -->
+
+<script setup>
+  import './auth.css'
+  import { useAuth } from '@/shared/useAuth'
+  import { ref } from 'vue'
+
+  const email = ref('')
+  const password = ref('')
+  const { login, logout, currentUser, loading, error } = useAuth()
+
+  const handleLogin = () => {
+    error.value = ''
+
+    login(email.value, password.value)
+  }
+</script>
 
 <template>
-  <div class=""></div>
+  <div class="auth-wrapper">
+    <div class="auth-box">
+      <h2>Login</h2>
+      <form @submit.prevent="handleLogin">
+        <input
+          autocomplete="email"
+          type="email"
+          v-model="email"
+          placeholder="Email"
+          required
+        />
+        <input
+          autocomplete="current-password"
+          type="password"
+          v-model="password"
+          placeholder="Password"
+          required
+        />
+
+        <!-- Error message -->
+        <p v-if="error" class="error-message">{{ error }}</p>
+
+        <!-- Login Button -->
+        <button
+          :class="{ 'loading-btn': loading }"
+          :disabled="loading"
+          type="submit"
+        >
+          Login
+        </button>
+      </form>
+
+      <!-- Switch to RegisterView -->
+      <p class="switch-auth">
+        Don't have an account?
+        <RouterLink to="/auth/register">Register</RouterLink>
+      </p>
+
+      <!--! Temporary buttons -->
+      <button
+        v-if="currentUser"
+        @click="console.log('current user:', currentUser)"
+      >
+        currentUser
+      </button>
+      <button v-if="currentUser" @click="logout">logout</button>
+      <!--! ------------------ -->
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,7 +1,67 @@
-<script setup></script>
+<!-- RegisterView.vue -->
+
+<script setup>
+  import './auth.css'
+  import { useAuth } from '@/shared/useAuth'
+  import { ref } from 'vue'
+
+  const email = ref('')
+  const password = ref('')
+  const confirmPassword = ref('')
+  const { createUser, loading, error } = useAuth()
+
+  const handleRegister = () => {
+    error.value = ''
+
+    if (password.value !== confirmPassword.value) {
+      error.value = 'Passwords do not match!'
+      return
+    }
+
+    createUser(email.value, password.value)
+  }
+</script>
 
 <template>
-  <div class=""></div>
+  <div class="auth-wrapper">
+    <div class="auth-box">
+      <h2>Register</h2>
+      <form @submit.prevent="handleRegister">
+        <input type="email" v-model="email" placeholder="Email" required />
+        <input
+          type="password"
+          v-model="password"
+          placeholder="Password"
+          required
+        />
+        <input
+          type="password"
+          v-model="confirmPassword"
+          :class="{ error: error === 'Passwords do not match!' }"
+          placeholder="Confirm Password"
+          required
+        />
+
+        <!-- Error message -->
+        <p v-if="error" class="error-message">{{ error }}</p>
+
+        <!-- Register Button -->
+        <button
+          :class="{ 'loading-btn': loading }"
+          :disabled="loading"
+          type="submit"
+        >
+          Register
+        </button>
+      </form>
+
+      <!-- switch to LoginView -->
+      <p class="switch-auth">
+        Already have an account?
+        <RouterLink to="/auth/login">Login</RouterLink>
+      </p>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
