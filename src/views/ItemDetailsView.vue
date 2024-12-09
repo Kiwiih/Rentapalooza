36076184b900de261c9c8ae65c1d9528438ac100
Paulina Kattilavaara <paulinakatt@hotmail.com>
2024-12-09 11:27:52 +0100

    Changed so the data that is displayed is the actual data and added a fallback image

--- src/views/ItemDetailsView.vue
+++ src/views/ItemDetailsView.vue
@@ -1,7 +1,26 @@
 <script setup>
 import { ref, computed } from 'vue';
+import { useItems } from '@/shared/useItems'
+import { useRoute } from 'vue-router';
 
-// This is a fake array that will eventually hold the items array.
+const { items, getItems } = useItems()
+const route = useRoute();
+
+getItems()
+
+const id = route.params.id;
+const currentId = computed(() => id - 1);
+
+const selectedItem = computed(() => {
+  return items.value.find(item => item.id === currentId.value);
+});
+
+const handleImageError = (event) => {
+  event.target.src = "https://company.rentitems.com/wp-content/uploads/2016/09/Rent-Items-Icon.png"
+}
+
+
+// This is a fake array I used before I got the real data.
 const itemsArray = ref([{
     id: "1",
     title: "Namn på item",
@@ -37,14 +56,12 @@ const itemsArray = ref([{
 
 
 // Function for getting the item with correct data from the itemsArray.
-const selectedId = ref("1"); 
-const selectedItem = computed(() => {
-  return itemsArray.value.find(item => item.id === selectedId.value);
-});
- console.log (selectedItem.value)
+// const selectedId = ref("1"); 
+// const selectedItem = computed(() => {
+//   return itemsArray.value.find(item => item.id === selectedId.value);
+// });
 
-
-// hard coded item while waiting for actual data
+// hard coded item I used while waiting for actual data.
 const item = ref({
     id: "1",
     title: "Namn på item",
@@ -64,21 +81,22 @@ const item = ref({
 </script>
 
 <template>
-    <div class="itemDetail">
-        <h1>{{  item.title }}</h1>
+
+    <div v-if="items.length > 0" class="itemDetail">
+        <h1>{{ items[currentId].title}}</h1>
 
         <div class="category">
-            <span v-for="(category, index) in item.category" :key="index">{{ category }}</span>
+            <span v-for="(category, index) in items[currentId].category" :key="index">{{ category }}</span>
         </div>
 
         <div class="imageDiv">
-            <img v-for="(image, index) in item.images" :key="index" :src="image" :alt="'Bild på item ' + (index + 1)" class="itemImage">
+            <img v-for="(image, index) in items[currentId].images" :key="index" :src="image" :alt="'Bild på item ' + (index + 1)" class="itemImage" @error="handleImageError">
         </div>
 
-        <p class="desc">{{ item.description }}</p>
-        <p class="price">{{  item.price }} kr</p>
-        <p :class="item.isAvailable ? 'available' : 'unavailable'">{{ item.isAvailable ? "Available for rent" : "Currently unavailable" }}</p>
-        <button v-if="item.isAvailable">Rent now!</button>
+        <p class="desc">{{ items[currentId].description }}</p>
+        <p class="price">{{  items[currentId].price }} kr</p>
+        <p :class="items[currentId].isAvailable ? 'available' : 'unavailable'">{{ items[currentId].isAvailable ? "Available for rent" : "Currently unavailable" }}</p>
+        <button v-if="items[currentId].isAvailable">Rent now!</button>
     </div>
 </template>
 
@@ -88,7 +106,7 @@ const item = ref({
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    margin: 1rem;
+    margin: 2rem;
     gap: 0.5rem;
 }
 
