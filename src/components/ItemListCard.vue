0c99783c46245402bc23b81d269077f3e0779f0a
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-12-10 11:23:34 +0100

    linkt to rigt page and also name on uothyrare

--- src/components/ItemListCard.vue
+++ src/components/ItemListCard.vue
@@ -1,61 +1,72 @@
 <script setup>
-const props = defineProps(['item'])
+  const props = defineProps(['item', 'ownerName'])
 
-const imageError = (event) => {
-    event.target.src = '/images/noImage.jpg';  // Ange här din fallback-bild
-};
+  const imageError = (event) => {
+    event.target.src = '/images/noImage.jpg' // Ange här din fallback-bild
+  }
 </script>
 
 <template>
-    <article>
-        <div class="img-container">
-            <img :src="props.item.images[0]" alt="Item Image" v-if="props.item.images?.length > 0" @error="imageError">
+  <article>
+    <div class="img-container">
+      <img
+        :src="props.item.images[0]"
+        alt="Item Image"
+        v-if="props.item.images?.length > 0"
+        @error="imageError"
+      />
+    </div>
+
+    <div class="card-content-container">
+      <div class="card-body">
+        <div class="card-body-main">
+          <h2>{{ item.title }}</h2>
+          <p>{{ item.description }}</p>
         </div>
 
-        <div class="card-content-container">
+        <div class="card-body-sidebar">
+          <small>
+            Outhyres by: <br />
+            <b>
+              <a href="#" style="text-decoration: none">{{
+                props.ownerName
+              }}</a>
+            </b>
+            (Todo: länka profil)
+          </small>
+          <p
+            :class="
+              item.isAvailable
+                ? 'badge available-badge'
+                : 'badge unavailable-badge'
+            "
+          >
+            {{
+              item.isAvailable ? 'Available for rent' : 'Not available for rent'
+            }}
+          </p>
+        </div>
+      </div>
 
-            <div class="card-body">
-                <div class="card-body-main">
-                    <h2>{{ item.title }}</h2>
-                    <p>{{ item.description }}</p>
-                </div>
-
-                <div class="card-body-sidebar">
-                    <small>
-                        Outhyres by: <br>
-                        <b> {{ item.ownerId }} (namn)</b>
-                    </small>
-                    <p :class="item.isAvailable ? 'badge available-badge' : 'badge unavailable-badge'">
-                        {{ item.isAvailable ? "Available for rent" : "Not available for rent" }}
-                    </p>
-                </div>
-            </div>
-
-
-            <div class="card-footer">
-                <div>
-                    <p class="pricetag"> {{ item.price }} Kr</p>
-                </div>
-
-                <div class="button-container">
-                    <button class="button-secondary">
-                        Read More
-                    </button>
-
-                    <button :disabled="!item.isAvailable">
-                        Rent Now
-                    </button>
-                </div>
-            </div>
-
+      <div class="card-footer">
+        <div>
+          <p class="pricetag">{{ item.price }} Kr</p>
         </div>
 
-    </article>
+        <div class="button-container">
+          <router-link :to="'/items/' + item.id">
+            <button class="button-secondary">Read More</button>
+          </router-link>
 
+          <button :disabled="!item.isAvailable">Rent Now</button>
+        </div>
+      </div>
+    </div>
+  </article>
 </template>
 
 <style scoped>
-article {
+  article {
     border: 1px solid transparent;
     border-radius: 0.5rem;
     padding: 0.5rem;
@@ -63,12 +74,10 @@ article {
     gap: 1rem;
     margin: 0.5rem 0rem;
 
-
-
     &:hover {
-        border: 1px solid var(--color-border);
-        background-color: hsla(0, 0%, 50%, 0.05)
-            /* 0.5 är transparensnivån */
+      border: 1px solid var(--color-border);
+      background-color: hsla(0, 0%, 50%, 0.05);
+      /* 0.5 är transparensnivån */
     }
 
     /* TRUNCATING AV ALLA TEXTER I KORTET. */
@@ -79,97 +88,91 @@ article {
     h4,
     h5,
     h6 {
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        -webkit-line-clamp: 2;
-        /* Begränsar till 2 rader */
-        overflow: hidden;
-        text-overflow: ellipsis
-            /* Visar "..." om texten är för lång */
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+      /* Begränsar till 2 rader */
+      overflow: hidden;
+      text-overflow: ellipsis;
+      /* Visar "..." om texten är för lång */
     }
 
     .img-container {
-        /* background-color: purple; */
-        flex-shrink: 0;
-        align-self: start;
-        width: clamp(2rem, 20%, 7rem);
-        aspect-ratio: 1 / 1;
+      /* background-color: purple; */
+      flex-shrink: 0;
+      align-self: start;
+      width: clamp(2rem, 20%, 7rem);
+      aspect-ratio: 1 / 1;
 
-        border: 2px solid var(--color-border);
-        border-radius: 0.25rem;
+      border: 2px solid var(--color-border);
+      border-radius: 0.25rem;
 
-        display: grid;
-        place-items: center;
-        overflow: hidden;
+      display: grid;
+      place-items: center;
+      overflow: hidden;
 
-        img {
-            width: 100%;
-            height: 100%;
+      img {
+        width: 100%;
+        height: 100%;
 
-            object-fit: cover;
-        }
+        object-fit: cover;
+      }
     }
 
-
-
-
     .card-content-container {
-        /* background-color: aquamarine; */
+      /* background-color: aquamarine; */
+      flex-grow: 1;
+
+      display: flex;
+      gap: 0.5rem;
+      flex-direction: column;
+
+      .card-body {
+        /* background-color: red; */
         flex-grow: 1;
 
-
         display: flex;
-        gap: 0.5rem;
-        flex-direction: column;
+        gap: 2rem;
 
-        .card-body {
-            /* background-color: red; */
-            flex-grow: 1;
-
-            display: flex;
-            gap: 2rem;
-
-            .card-body-main {
-                /* background-color: blueviolet; */
-                flex-grow: 1;
-            }
-
-            .card-body-sidebar {
-                /* background-color: blue; */
-                width: clamp(5rem, 33%, 15rem);
-                flex-shrink: 0;
-                text-align: right;
-                line-height: 1rem;
-
-                display: flex;
-                flex-direction: column;
-                justify-content: space-between;
-                align-items: end;
-
-
-            }
+        .card-body-main {
+          /* background-color: blueviolet; */
+          flex-grow: 1;
         }
 
-        .card-footer {
-            display: flex;
-            justify-content: space-between;
-            /* background-color: aqua; */
+        .card-body-sidebar {
+          /* background-color: blue; */
+          width: clamp(5rem, 33%, 15rem);
+          flex-shrink: 0;
+          text-align: right;
+          line-height: 1rem;
+
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-items: end;
         }
+      }
+
+      .card-footer {
+        display: flex;
+        justify-content: space-between;
+        /* background-color: aqua; */
+      }
     }
 
     .pricetag {
-        font-size: x-large;
-        font-weight: bold;
-        flex-grow: 1;
-        width: max-content;
+      font-size: x-large;
+      font-weight: bold;
+      flex-grow: 1;
+      width: max-content;
     }
 
     .button-container {
-        text-align: right;
+      text-align: right;
     }
 
     button {
-        margin-left: 0.5rem;
+      margin-left: 0.5rem;
     }
-}
+  }
 </style>
