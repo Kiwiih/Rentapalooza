aca684a5f4a7c9c6cd98beeff1dcaaf8d3f3744e
jerry <jerry.olsson@chasacademy.se>
2024-12-12 19:15:13 +0100

    import JSONbin URLs from enviroment file

--- src/shared/useItems.js
+++ src/shared/useItems.js
@@ -1,53 +1,49 @@
-import { ref } from "vue";
-import axios from "axios";
+import { ref } from 'vue'
+import axios from 'axios'
 
 // Denna variabel håller våra Items från databasen
 // Kan användas globalt för att se den data som finns.
-const items = ref([]);
+const items = ref([])
 
 export function useItems() {
-
-
-
-
-    // Denna funktion hämtar från databasen och placerar resultatet i items-variabeln
-    // Kllas på för att uppdatera items- variabeln
-    const getItems = async () => {
-        try {
-            const response = await axios.get('https://api.jsonbin.io/v3/b/6751aef2e41b4d34e46057f5', {
-                headers: {
-                    'X-Master-Key': import.meta.env.VITE_API_X_MASTER_KEY,
-                    'Content-Type': 'application/json'
-                }
-            });
-            items.value = response.data.record.items;
-        } catch (error) {
-            console.error("Fel vid hämtning av data med Axios:", error);
+  // Denna funktion hämtar från databasen och placerar resultatet i items-variabeln
+  // Kllas på för att uppdatera items- variabeln
+  const getItems = async () => {
+    try {
+      const response = await axios.get(import.meta.env.VITE_API_ITEMS_URL, {
+        headers: {
+          'X-Master-Key': import.meta.env.VITE_API_X_MASTER_KEY,
+          'Content-Type': 'application/json'
         }
-        console.log("GET ITEMS -hämtning genomförd")
-    };
-
-    const updateItems = async (updatedItems) => {
-        try {
-            await axios.put(
-                'https://api.jsonbin.io/v3/b/6751aef2e41b4d34e46057f5',
-                { items: updatedItems },
-                {
-                    headers: {
-                        'X-Master-Key': import.meta.env.VITE_API_X_MASTER_KEY,
-                        'Content-Type': 'application/json'
-                    },
-                }
-            );
-            items.value = updatedItems;
-        } catch (error) {
-            console.error(error);
-        }
+      })
+      items.value = response.data.record.items
+    } catch (error) {
+      console.error('Fel vid hämtning av data med Axios:', error)
     }
+    console.log('GET ITEMS -hämtning genomförd')
+  }
 
-    return {
-        items,
-        getItems,
-        updateItems,
-    };
+  const updateItems = async (updatedItems) => {
+    try {
+      await axios.put(
+        import.meta.env.VITE_API_ITEMS_URL,
+        { items: updatedItems },
+        {
+          headers: {
+            'X-Master-Key': import.meta.env.VITE_API_X_MASTER_KEY,
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+      items.value = updatedItems
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return {
+    items,
+    getItems,
+    updateItems
+  }
 }
