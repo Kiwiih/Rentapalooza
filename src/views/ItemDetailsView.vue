54d65f3d38dc0182c286cccd59e421ac8252bf46
Viktor Magnusson <viktor.kuddis@hotmail.com>
2024-12-12 16:24:06 +0100

    items vue fungerar nu även om man inte tidigare klickat runt i spa

--- src/views/ItemDetailsView.vue
+++ src/views/ItemDetailsView.vue
@@ -1,16 +1,42 @@
 <script setup>
   import { useItems } from '@/shared/useItems'
   import { useRoute } from 'vue-router'
-  import BookingButton from '@/components/BookingButton.vue';
+  import BookingButton from '@/components/BookingButton.vue'
+  import { ref, watch } from 'vue'
 
-  const { items } = useItems()
+  const { items, getItems } = useItems()
   const route = useRoute()
 
   // get params from url
   const id = route.params.id
 
-  // Find selected item from items array
-  const selectedItem = items.value.find((item) => item.id === id)
+  /*************
+   I de fall då man går direkt in på items details via länk och kommer utifrån SPA så kommer vi behöva hämta items innan vi kan göra något då items inte fyllts på ännu... därför skapar vi en ref för de selectade itemet som templaten kan vara beroende av och avgöra om den är redo att renderas eller ej....
+  *************/
+
+  // store the current item that we r gonna show on page
+  const selectedItem = ref('')
+
+  // om items saknar innehåll så hämtar vi innehåll
+  if (items.value <= 0) {
+    getItems()
+  } else {
+    // annars plockar vi ut rätt item...
+    selectedItem.value = items.value.find((item) => item.id === id)
+  }
+
+  // Lyssna på förändringar i items.
+  // OBS. funktionen körs även en gång inledningsvis! pga immediate = true
+  // om items förändras.. ex går från tom till att ha innehåll... såååå.......
+  watch(
+    () => items.value, //vi bevakar detta värdet
+    (newItems) => {
+      if (newItems.length > 0) {
+        selectedItem.value = newItems.find((item) => item.id === id)
+      }
+    },
+    { immediate: true } // kör en gång inledningsvis också
+  )
 
   const handleImageError = (event) => {
     event.target.src =
@@ -19,7 +45,7 @@
 </script>
 
 <template>
-  <div v-if="items.length > 0" class="itemDetail">
+  <div v-if="selectedItem" class="itemDetail">
     <h1>{{ selectedItem.title }}</h1>
 
     <div class="category">
@@ -48,9 +74,8 @@
           : 'Currently unavailable'
       }}
     </p>
-    
-        <BookingButton :item="selectedItem" />
 
+    <BookingButton :item="selectedItem" />
   </div>
 </template>
 
