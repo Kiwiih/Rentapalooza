40940402f87e9e49b1d1a4063e54322a27b7d121
Alicia Karlsson <144915365+aliciakarlsson@users.noreply.github.com>
2024-12-17 10:34:11 +0100

    Revert "no button if you r owning the iten so now you not get comfused wy you…"

--- src/components/BookingButton.vue
+++ src/components/BookingButton.vue
@@ -83,28 +83,28 @@
 </script>
 
 <template>
-  <div v-if="currentUser">
+  <div>
     <button
-      v-if="item.ownerId != currentUser.id"
       @click="bookItem(item)"
       :class="{
         'loading-btn': item && loadingId === item.id,
-        'need-to-login-btn': !currentUser
+        'owned-item-btn': currentUser && item.ownerId === currentUser.id,
+        'need-to-login-btn': !currentUser,
       }"
       :disabled="!item.isAvailable || loadingId === item.id"
     >
       {{ loadingId === item.id ? 'Loading...' : 'Book item' }}
     </button>
-
-    <div v-else class="badge your-item-mark">Your item</div>
   </div>
 </template>
 
 <style scoped>
-  .your-item-mark {
-    color: var(--color-text-muted);
-    margin-top: 0.5rem;
+  .owned-item-btn {
+    background-color: red;
+    color: white;
+    cursor: not-allowed;
   }
+
   .need-to-login-btn::before {
     content: 'You need to log in';
     padding: 1rem;
