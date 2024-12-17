f0e8da9e6dce4fcdcc18c4f1adcfffac38770a5b
jerry <jerry.olsson@chasacademy.se>
2024-12-17 13:56:44 +0100

    now its possible to update Rentals

--- src/shared/useRentals.js
+++ src/shared/useRentals.js
@@ -6,6 +6,7 @@ const error = ref(null)
 const loading = ref(false)
 
 export const useRentals = () => {
+  // * Fetch all Rentals
   const fetchRentals = async () => {
     error.value = null
     loading.value = true
@@ -25,6 +26,7 @@ export const useRentals = () => {
     }
   }
 
+  // * Add new Rental
   const addRental = async (newRental) => {
     error.value = null
     loading.value = true
@@ -54,11 +56,60 @@ export const useRentals = () => {
     }
   }
 
+  // * Update Rental
+  const updateRental = async (rentalId, updatedFields) => {
+    loading.value = true
+    error.value = null
+
+    try {
+      // Start with fetching all rentals
+      // await fetchRentals()
+
+      // Find rental that should be updated
+      const rentalIndex = rentals.value.findIndex(
+        (rental) => rental.id === rentalId
+      )
+
+      // if rental can't be found
+      if (rentalIndex === -1) {
+        throw new Error('Rental could not be found')
+      }
+
+      // Update rental fields
+      const updatedUser = { ...rentals.value[rentalIndex], ...updatedFields }
+
+      // Update list with the updated rental
+      rentals.value[rentalIndex] = updatedUser
+
+      // Send updated data to the Api
+      const response = await axios.put(
+        import.meta.env.VITE_API_RENTALS_URL,
+        { rentals: rentals.value },
+        {
+          headers: {
+            'X-Master-Key': import.meta.env.VITE_API_X_MASTER_KEY,
+            'Content-Type': 'application/json'
+          }
+        }
+      )
+
+      rentals.value = await response.data.record.rentals
+      console.log('Rental updated successfully')
+    } catch (err) {
+      error.value = err.message || 'A problem occured while updating rentals'
+      console.error(err)
+    } finally {
+      loading.value = false
+    }
+  }
+
   return {
     rentals,
     error,
     loading,
+
     fetchRentals,
-    addRental
+    addRental,
+    updateRental
   }
 }
